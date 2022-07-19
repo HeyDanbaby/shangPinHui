7384cb0dc06dad0d69bf4a4c6c60969db57ede0d
陈晶丹 <2972463088@qq.com>
2022-07-19 18:22:33 +0800

    add

--- src/api/request.js
+++ src/api/request.js
@@ -1,4 +1,12 @@
 import axios from 'axios'
+
+// 引入进度条
+import nprogress from 'nprogress'
+// console.log(nprogress);
+// start()：进度条开始  done()：进度条结束
+// 引入进度条样式
+import 'nprogress/nprogress.css'
+
 // 对axios进行二次封装
 // 1、利用axios对象的方法create，创建一个axios实例
 // 2、把创建的axios实例 赋值 给requests
@@ -13,18 +21,22 @@ const requests = axios.create({
 
 // 请求拦截器：再发请求之前，请求拦截器可以监测到。可以在服务器发出去之前做一些事情
 requests.interceptors.request.use((config) => {
+  // 进度条开始动
+  nprogress.start();
   // config：配置对象 对象里面有个属性很重要：headers请求头
   return config
 })
 
 // 响应拦截器
-requests.interceptors.response.use(() => {
+requests.interceptors.response.use((res) => {
+  // 进度条结束
+  nprogress.done();
   // 请求成功的回调函数，服务器响应数据回来以后，响应拦截器可以监测到，可以做一些事情
   return res.data;
-},(err) => {
+}, (err) => {
   // 请求失败的回调函数
-  return Promise.reject(new Error('faile'))  
-})  
+  return Promise.reject(new Error('faile'))
+})
 
 // 对外暴露
 export default requests;
